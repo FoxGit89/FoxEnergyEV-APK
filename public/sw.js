463c4b165fc789dd0915be9a985a53fcb239f2c8
FoxGit89 <fox_git@outlook.com>
2026-04-26 01:44:17 +0200

    V51

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'foxsync-v50';
+const CACHE_NAME = 'foxsync-v51';
 const ASSETS = [
   './',
-  './index.html?v=50',
-  './style.css?v=50',
-  './app.js?v=50',
-  './manifest.json?v=50',
+  './index.html?v=51',
+  './style.css?v=51',
+  './app.js?v=51',
+  './manifest.json?v=51',
   'https://cdn.jsdelivr.net/npm/chameleon-ultra.js@0/dist/index.global.js',
   'https://cdn.jsdelivr.net/npm/chameleon-ultra.js@0/dist/Crypto1.global.js',
   'https://cdn.jsdelivr.net/npm/chameleon-ultra.js@0/dist/plugin/WebbleAdapter.global.js'
